1834a87450ec2ada9d0b05b96e5c99a7b0f1bf8f
John <jtwolbert52@gmail.com>
2025-09-09 14:24:57 -0400

    second commit

---
+++ EECS1030_lab2_wolbert.java
@@ -0,0 +1,9 @@
+// Created by John Wolbert on 2025-09-09
+
+package EECS1030_lab2_wolbert;
+
+public class EECS1030_lab2_wolbert {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
